9da813691791439c5064762879d8639fe2e02ef2
Moyo06 <moyo.iyanda@pau.edu.ng>
2026-07-20 13:38:37 +0100

    Update Number_guessing_game.py

--- Week1/Number_guessing_game.py
+++ Week1/Number_guessing_game.py
@@ -7,18 +7,23 @@ secret_number = random.randint(1, 100)
 print("Guess between number 1 to 100")
 
 attempts = 0
+
 while True:
+   try:
+      guess = int(input("Enter a number between 1 and 100"))
+   except ValueError:
+      print("Enter a valid whole number")
+      continue
 
- guess = int(input("Enter a number between 1 and 100"))
 
-
- attempts +=1
- if guess < secret_number:
-    print("Too Low! Try again.")
- elif guess > secret_number:
-    print("Too High! Guess again.")
- elif guess == secret_number:
-    print("Congratulations! You got the correct number!")
-    break
+   attempts +=1
+   
+   if guess < secret_number:
+      print("Too Low! Try again.")
+   elif guess > secret_number:
+      print("Too High! Guess again.")
+   elif guess == secret_number:
+      print("Congratulations! You got the correct number!")
+      break
 print(f"You guessed the number in {attempts} attempts!")
  
